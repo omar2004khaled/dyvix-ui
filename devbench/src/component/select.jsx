4bd5ad4b8b5e8bce38142a35d87273861e3f17ff
priyan923 <priyanshisaini615@gmail.com>
2026-07-09 08:10:12 +0530

    Updated select.jsx
    Removed comments about theme usage and style handling.

--- devbench/src/component/select.jsx
+++ devbench/src/component/select.jsx
@@ -8,9 +8,8 @@ export function SelectTest() {
   return (
     <DyvixSelect
       animation={DYVIX_GLOBAL_ANIMATION.GLITCH}
-      theme="Blade"  // Using your new theme
+      theme="Blade"  
       placeholder={'number'}
-      // background and dropdownBackground removed to let the theme handle styles
       className="ex-select"
       type="select"
       elements={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
